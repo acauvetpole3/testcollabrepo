738e40ca06075e753c4bc82fd447739dd7b80974
Audric-Jai Cauvet <40215674+acauvetpole3@users.noreply.github.com>
2020-06-17 17:41:19 +0530

    Added second light restiistor

--- LEDloop.py
+++ LEDloop.py
@@ -14,12 +14,19 @@ import time
 import sys
 import logging
 
-def main(lr):
+def main(lr, lr2=None):
     logging.info("Staring main loop")
     try:
         lr_int = int(lr)
         photoresistor = MCP3008(lr_int)
+        if lr2 is not None:
+            lr2_int = int(lr2)
+            photoresistor2 = MCP3008(lr2_int)
         while True:
+            if lr2 is not None:
+                outstring = "lr{0}:{1}|lr{2}:{3}".format(lr,photoresistor.value,lr2,photoresistor2.value)
+            else:
+                outstring = "lr{0}:{1}".format(lr,photoresistor.value)
             print (photoresistor.value)
             time.sleep(.03)
     except KeyboardInterrupt:
@@ -34,14 +41,19 @@ def main(lr):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
+    lr = 0
+    lr2 = None
+    if len(sys.argv) == 3:
+        lr = sys.argv[1]
+        lr2 = sys.argv[2]
+    elif len(sys.argv) == 2:
         lr = sys.argv[1]
     elif len(sys.argv) == 1:
         lr = 0
     else:
         print "{0} syntax: python {0} [photoresistor_id]"
     try:
-        sys.exit(main(lr))
+        sys.exit(main(lr, lr2))
     except KeyboardInterrupt:
         # handle keyboard interrupt #
         logging.info("Exiting LEDLoop.py")
